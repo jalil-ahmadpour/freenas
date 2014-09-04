c75c0954962abb3464458c786ba05703669d4a3b
Charlie Root <root@builder-nas10.jbase.com>
2014-09-04 23:56:36 +0430

    os-base

--- build/nanobsd-cfg/os-base-functions.sh
+++ build/nanobsd-cfg/os-base-functions.sh
@@ -419,6 +419,22 @@ last_orders() {
 		echo "${sha256_signature}" > ${NANO_IMGNAME}.GUI_Upgrade.txz.sha256.txt
 		)
 	fi
+	
+	pprint 2 "Creating VMDK"
+	log_file "${vmdk_image_log}"
+
+	(
+	set -x
+	if [ -f /usr/local/bin/VBoxManage ]; then
+		VBoxManage convertfromraw "$NANO_DISKIMGDIR/$NANO_IMGNAME" "${vmdk_image}" --format VMDK
+		chmod 644 "${vmdk_image}"
+		time ${NANO_XZ} ${PXZ_ACCEL} --verbose -6 -f "${vmdk_image}"
+		sha256_signature=`sha256 ${vmdk_image_compressed}`
+		echo "${sha256_signature}" > ${vmdk_image_compressed}.sha256.txt
+	else
+		echo "VBoxManage not found"
+	fi
+	) > "${vmdk_image_log}" 2>&1
 
 }
 
